37dac5f1060c18a61ed38faf6b81630e9e6be883
GuchenkoSI <mr.guchenko@mail.ru>
2023-06-09 10:23:09 +0300

    Исправления к домашней работе

--- tests/test_lesson_2.py
+++ tests/test_lesson_2.py
@@ -4,5 +4,5 @@ from selene import be, have
 
 def test_lesson_2(set_window_size):
     browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('QA.GURU').press_enter()
-    browser.element('[id="search"]').should(have.text('QA.GURU — Курсы тестировщиков - онлайн-обучение'))
+    browser.element('[name="q"]').should(be.blank).type('bgdbfgbdbrtddb').press_enter()
+    browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
